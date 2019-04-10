2a56ebcf5f20f9ad744182d3a68d69af4fcd8628
Amoto1103 <qiuwcsh@126.com>
2019-04-10 21:54:36 +0800

    新架构
    子窗口连接好了，往里面写函数就行啦

--- main_func.py
+++ main_func.py
@@ -1,6 +1,19 @@
 import sys
 from main_interface import Ui_Dialog
+from add_interface import Ui_Dialog_add
 from PyQt5 import QtWidgets, QtGui
+
+class add_window(QtWidgets.QWidget, Ui_Dialog_add):
+    def __init__(self):
+        super(add_window, self).__init__()
+        self.setupUi(self)
+
+    def confirm(self):
+        print ("confirm")
+
+    def cancel(self):
+        print ("cancel")
+
 
 class father_window(QtWidgets.QWidget, Ui_Dialog):
     def __init__(self):
@@ -8,7 +21,9 @@ class father_window(QtWidgets.QWidget, Ui_Dialog):
         self.setupUi(self)
 
     def add(self):
-        print ("add")
+        self.add_app = QtWidgets.QApplication(sys.argv)
+        self.add_show = add_window()
+        self.add_show.show()
 
     def delete(self):
         print ("delete")
